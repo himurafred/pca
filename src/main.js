2fd0c57840af98e81ff0ea24f2bdb44302fb1d14
himurafred <himurafred@gmail.com>
2017-11-19 17:06:12 +0100

    import

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import 'onsenui';
 require('onsenui/css-components-src/src/onsen-css-components.css');
 require('onsenui/css/onsenui.css');
 
-import App from './App';
+import App from './pages/App';
 
 Vue.use(VueOnsen);
 
